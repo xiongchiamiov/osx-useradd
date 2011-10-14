f973425764f3099f146c96df10a68754a0c1052d
xiongchiamiov <xiong.chiamiov@gmail.com>
2011-10-14 09:18:09 -0700

    report *which* command isn't yet implemented

--- osx-useradd.rb
+++ osx-useradd.rb
@@ -6,107 +6,107 @@ OptionParser.new do |opts|
 	opts.banner = "Usage: #{$0} [options] LOGIN"
 	
 	opts.on('-b', '--base-dir BASE_DIR') do |baseDir|
-		puts 'Not yet implemented.'
+		puts '-b not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-c', '--comment COMMENT') do |comment|
-		puts 'Not yet implemented.'
+		puts '-c not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-d', '--home HOME_DIR') do |homeDir|
-		puts 'Not yet implemented.'
+		puts '-d not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-D', '--default') do
-		puts 'Not yet implemented.'
+		puts '-D not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-e', '--expiredate EXPIRE_DATE') do |expireDate|
-		puts 'Not yet implemented.'
+		puts '-e not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-f', '--inactive INACTIVE') do |inactive|
-		puts 'Not yet implemented.'
+		puts '-f not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-g', '--gid GROUP') do |group|
-		puts 'Not yet implemented.'
+		puts '-g not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-G', '--groups GROUP1[,GROUP2,...[,GROUPN]]', Array) do |groups|
-		puts 'Not yet implemented.'
+		puts '-G not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-k', '--skel SKEL_DIR') do |skelDir|
-		puts 'Not yet implemented.'
+		puts '-k not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-K', '--key KEY=VAUE') do |key|
-		puts 'Not yet implemented.'
+		puts '-K not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-l', '--no-log-init') do
-		puts 'Not yet implemented.'
+		puts '-l not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-m', '--create-home') do
-		puts 'Not yet implemented.'
+		puts '-m not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-M') do
-		puts 'Not yet implemented.'
+		puts '-M not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-N', '--no-user-group') do
-		puts 'Not yet implemented.'
+		puts '-N not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-o', '--non-unique') do
-		puts 'Not yet implemented.'
+		puts '-o not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-p', '--password PASSWORD') do |password|
-		puts 'Not yet implemented.'
+		puts '-p not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-r', '--system') do
-		puts 'Not yet implemented.'
+		puts '-r not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-s', '--shell SHELL') do |shell|
-		puts 'Not yet implemented.'
+		puts '-s not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-u', '--uid UID') do |uid|
-		puts 'Not yet implemented.'
+		puts '-u not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-U', '--user-group') do
-		puts 'Not yet implemented.'
+		puts '-U not yet implemented.'
 		exit 1
 	end
 
 	opts.on('-Z', '--selinux-user SEUSER') do |seUser|
-		puts 'Not yet implemented.'
+		puts '-Z not yet implemented.'
 		exit 1
 	end
 end.parse!
